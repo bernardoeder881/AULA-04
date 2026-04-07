43937a9af98acd82e21e335f1941f575ac5b37b2
bernardoeder881 <bernardoeder881>
2026-04-07 20:28:19 -0300

    atividade 01 finalizada

--- atividade01.py
+++ atividade01.py
@@ -1,11 +1,13 @@
-valor = float(input('Insira o valor'))
+valor = float( input('Insira o valor: '))
 
 if valor > 250:
     desconto = valor*0.16
+    valor_final = valor - desconto
+    print(f"O Valor final é de R$ {valor:.2f}, e com R${desconto:.2f} de desconto fica no valor de R${valor_final:.2f}")
 else:
     desconto = 0
-
-valor_final = valor - desconto
-print(desconto)
-print(f"O Valor final é de R${valor_final} ")
+    valor_final = valor - desconto
+    print("Não há desconto")
+    print(f"O Valor final é de R${valor_final:.2f}")
       
+
